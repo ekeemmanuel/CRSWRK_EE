b485d12243b3f89a3b09b373cef1f1111317bf51
ekeemmanuel <ekeemmanuel@gmail.com>
2017-04-17 07:03:34 +0100

    Monday

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include("connection.php");
-if (!isset($_SESSION['firstName'])) {
+if (!isset($_SESSION['userSession'])) {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $username = $_POST['username'];
         $password = $_POST['password'];
