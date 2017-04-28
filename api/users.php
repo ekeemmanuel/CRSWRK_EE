7a22ed06b78314e73b41ebd5d9ee6614f9f9b825
ekeemmanuel <ekeemmanuel@gmail.com>
2017-04-28 18:49:58 +0100

    Fridh

--- api/users.php
+++ api/users.php
@@ -67,8 +67,9 @@ function get_users($user_id)
     global $connection;
     $query="SELECT * FROM users";
     if($user_id)
+    echo $user_id;
     {
-        $query .= ' WHERE userID="ST000002" LIMIT 1';
+        $query .= ' WHERE userID="' . $user_id . '" LIMIT 1';
     }
     $response=array();
     $result=mysqli_query($connection, $query);
