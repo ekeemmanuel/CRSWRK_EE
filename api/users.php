65252fc018848024d855331eaececb234d1bcbb2
ekeemmanuel <ekeemmanuel@gmail.com>
2017-04-30 23:50:18 +0100

    sunday

--- api/users.php
+++ api/users.php
@@ -47,7 +47,7 @@ function get_users($userid)
 
     $response=array();
     $result=mysqli_query($connection, $query);
-    while($row=mysqli_fetch_array($result))
+    while($row=mysqli_fetch_assoc($result))
     {
         $response[]=$row;
     }
